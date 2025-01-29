7d7254a35ee12ec62dcae84d106b2aa2ed89f6a8
Paradox Shadow <testa7294@gmail.com>
2025-01-28 23:42:43 -0800

    Almost final results

--- javaScript/script.js
+++ javaScript/script.js
@@ -3,26 +3,30 @@ document.addEventListener('DOMContentLoaded', () => {
     const emailError = document.getElementById('email-error');
     const emailField = document.getElementById('email');
 
-    const newsLetterCard = document.getElementById('newsletter-form-card');
-    const thankYouMessageCard = document.getElementById('thankyou-message-card');
+    const newsLetterCard = document.querySelector('.newsletter-form-card');
+    const thankYouMessageCard = document.querySelector('.thankyou-message-card');
     const userEmail = document.getElementById('user-email');
 
     const dismissBtn = document.querySelector('.btn.dismiss');
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         if (emailField.validity.valid) {
+            emailError.classList.add('hidden'); //Hide error field
             emailError.textContent = ''; // Clear the error message if valid
             userEmail.textContent = emailField.value; //Change the value to display the user email.
+            emailField.classList.remove('invalid'); //Remove the invalid class from the emailField
             toggleCards();
         } else {
+            emailField.classList.add('invalid'); //Add the invalid class to the emailField
             showError(emailField); // Display the error message
         }
     });
     function showError(field) {
+        emailError.classList.remove('hidden'); //Show the error field
         if (field.validity.valueMissing) {
-            emailError.textContent = 'Please enter your email address.';
+            emailError.textContent = 'Valid email required.';
         } else if (field.validity.typeMismatch) {
-            emailError.textContent = 'Please enter a valid email address.';
+            emailError.textContent = 'Valid email required.';
         }
     }
 
@@ -30,6 +34,7 @@ document.addEventListener('DOMContentLoaded', () => {
         newsLetterCard.classList.toggle('hidden');
         thankYouMessageCard.classList.toggle('hidden');
     }
+
     dismissBtn.addEventListener('click', () => {
         emailField.value = '';
         toggleCards();
